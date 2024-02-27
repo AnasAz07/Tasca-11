baa056e02bb14787d800856cbdca6940087d612b
AnasAz07 <aazaroual20745@iesjoanramis.org>
2024-02-27 12:32:59 +0100

    ex 3

--- ex3.py
+++ ex3.py
@@ -4,9 +4,15 @@ def llegir_llista():
     while a!= '.':
         a = input("Introduce la lista, para terminarla mete un . : ")
         if a!= '.':
-            l.append(int(a))
+            l.append(a)
         else:
             return l
         
-def filtre_inicial():
-    
+def filtre_inicial(x,a,b):
+    l = list(filter(lambda y: y[0] == a or y[0] == b, x))
+    return l
+
+x = llegir_llista()
+a = "m"
+b = "p"
+print(filtre_inicial(x,a,b))
