c8688e664bf7a6bd8206d6ce1eca2fc047a419e8
AnasAz07 <aazaroual20745@iesjoanramis.org>
2024-03-05 12:57:56 +0100

    ex12

---
+++ ex12.py
@@ -0,0 +1,20 @@
+import os
+
+companys = ["Federic","Oscar","Paula","Hugo","Claudia","Jordi","Anas","Alvaro","Clara","Palli","Joan","Marc","GitCalvo","Sebas","Pepe","Joselu","Ayoub","David"]
+os.mkdir("/home/cicles/AO/Tasca-11/Prova")
+os.chdir("/home/cicles/AO/Tasca-11/Prova")
+with open("ex12.txt","a+") as f:
+    for e in companys:
+        f.write(e+"\n")
+professors = ["Pep","David","Fela","Lluis","Joan"]
+with open("ex12.txt","a+") as f:
+    for e in professors:
+        f.write(e+"\n")
+
+a = []
+with open("ex12.txt","r") as f:
+    for e in f:
+        c = e.split("\n")
+        a.append(c[0])
+
+print(a)
